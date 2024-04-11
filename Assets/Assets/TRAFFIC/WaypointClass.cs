3723d5c68ef004f6afefc63a5aea1280151b941b
Daniel Rossing <drossi20@student.aau.dk>
2024-04-11 12:45:52 +0200

    Traffic System, the whole thing + scene
    Yuh

---
+++ Assets/Assets/TRAFFIC/WaypointClass.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointClass : MonoBehaviour
+{
+    public bool isWaitingPoint = false;
+    public float waitingTime = 5f;
+}
