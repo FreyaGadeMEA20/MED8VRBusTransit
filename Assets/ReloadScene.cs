99ec719b2cabd37d614bb49ae1315ae7116c893b
NickolaiFSJ <nfsj20@student.aau.dk>
2024-04-23 20:43:28 +0200

    nu kan man dø

--- Assets/ReloadScene.cs
+++ Assets/ReloadScene.cs
@@ -1,20 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ReloadScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-
+            Debug.Log("Reloading Level");
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadSceneAsync(currentSceneName);
         }
     }
 }
