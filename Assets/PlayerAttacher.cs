86bf1dd000df01cc58add3894c296bc79237280e
Lukas Gade Ravnsborg <lravns20@student.aau.dk>
2024-05-02 21:01:28 +0200

    Parent work
    ...

--- Assets/PlayerAttacher.cs
+++ Assets/PlayerAttacher.cs
@@ -1,27 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;   
 
 public class PlayerAttacher : MonoBehaviour
 {
     [SerializeField] GameObject bus;
+
+    [SerializeField] ParentConstraint _constrainer;
     // Start is called before the first frame update
+    ConstraintSource constraintSource;
     void Start()
     {
-        
+        _constrainer = GameObject.Find("XR Player").GetComponent<ParentConstraint>();
+        constraintSource.sourceTransform = bus.transform; 
+        constraintSource.weight = 1;
+        _constrainer.AddSource(constraintSource);
     }
 
     void OnTriggerEnter(Collider other){
         if(other.tag == "Player"){
-            other.transform.parent = bus.transform;
+            //other.transform.parent = bus.transform;
             Debug.Log("Attached");
+            _constrainer.constraintActive = true;
         }
     }
 
     void OnTriggerExit(Collider other){
         if (other.tag == "Player"){
-            other.transform.parent = null;
+            //other.transform.parent = null;
             Debug.Log("Dettached");
+            _constrainer.constraintActive = false;
         }
     }
 }
--- Assets/StopScript.cs
+++ Assets/StopScript.cs
@@ -7,6 +7,10 @@ public class StopScript : MonoBehaviour
     public BusScreenController busScreenController;
     [SerializeField] BusGameManager busGameManager;
 
+    void Start(){
+        busGameManager = GameObject.Find("GameManager").GetComponent<BusGameManager>();
+    }
+
     public void StopButton(){
         busGameManager.StopBus();
         busScreenController.ApplyStopTexture();
